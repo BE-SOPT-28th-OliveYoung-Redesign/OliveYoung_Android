bcd4e380a0cf73c8e4754b232567909f2e7cfdbe
JooYae <jooyae3970@naver.com>
2021-05-15 15:47:53 +0900

    base settings

---
+++ app/src/main/java/org/sopt/designclient_oliveyoung/ReviewActivity.kt
@@ -0,0 +1,11 @@
+package org.sopt.designclient_oliveyoung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review)
+    }
+}
