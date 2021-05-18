4d4ef000f530d3db55933dfdc947200b0bd1733f
Kim Seojin <001106ksj@gmail.com>
2021-05-18 17:08:31 +0900

    [Feat] Review Layout 구성

--- app/src/main/java/org/sopt/designclient_oliveyoung/ReviewActivity.kt
+++ app/src/main/java/org/sopt/designclient_oliveyoung/ReviewActivity.kt
@@ -2,10 +2,23 @@ package org.sopt.designclient_oliveyoung
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class ReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+        val reviewerPropertiesChipGroup : ChipGroup = findViewById(R.id.chip_group_reviewer_properties)
+        val reviewerProperties = arrayOf("복합성", "쿨톤", "각질", "모공")
+        for (i in reviewerProperties) {
+            val newChip = Chip(this@ReviewActivity)
+            newChip.text = i
+            newChip.setTextColor(resources.getColor((R.color.mint)))
+            newChip.setChipBackgroundColorResource(R.color.white)
+            newChip.setChipStrokeColorResource(R.color.mint)
+            newChip.setChipStrokeWidthResource(R.dimen.chip_stroke_width)
+            reviewerPropertiesChipGroup.addView(newChip)
+        }
     }
 }
