162f830fd34fb090bd08dd551fed49aa793c8998
Kim Seojin <001106ksj@gmail.com>
2021-06-10 02:50:44 +0900

    [Feat] Click Event 추가

--- app/src/main/java/org/sopt/designclient_oliveyoung/home/view/HomeItemAdapter.kt
+++ app/src/main/java/org/sopt/designclient_oliveyoung/home/view/HomeItemAdapter.kt
@@ -1,11 +1,14 @@
 package org.sopt.designclient_oliveyoung.home.view
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import org.sopt.designclient_oliveyoung.R
 import org.sopt.designclient_oliveyoung.databinding.ItemRankingBinding
+import org.sopt.designclient_oliveyoung.review.ReviewActivity
 
 class HomeItemAdapter(private val items: List<RankingResponse.Data>) :
     RecyclerView.Adapter<HomeItemAdapter.HomeItemViewHolder>() {
@@ -34,6 +37,12 @@ class HomeItemAdapter(private val items: List<RankingResponse.Data>) :
                 binding.item = item
                 Glide.with(binding.imgItemRanking.context).load(item.mainImageView)
                     .into(binding.imgItemRanking)
+                binding.root.setOnClickListener {
+                    val intent = Intent(binding.root.context, ReviewActivity::class.java).apply {
+                        putExtra("itemRank", item.rank)
+                    }
+                    startActivity(binding.root.context, intent, null)
+                }
             } else {
                 binding.apply {
                     imgItemRanking.setImageResource(R.drawable.banner1)
