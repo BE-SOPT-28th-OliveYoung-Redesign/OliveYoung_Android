7b02ade372b878116ff7eba656ba1a6afa920cc3
Kim Seojin <001106ksj@gmail.com>
2021-06-10 02:40:30 +0900

    [Feat] Retrofit 통신 추가, Response Body와 Recyclerview 연결

--- app/src/main/java/org/sopt/designclient_oliveyoung/home/view/HomeActivity.kt
+++ app/src/main/java/org/sopt/designclient_oliveyoung/home/view/HomeActivity.kt
@@ -3,12 +3,23 @@ package org.sopt.designclient_oliveyoung.home.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import org.sopt.designclient_oliveyoung.databinding.ActivityHomeBinding
 import org.sopt.designclient_oliveyoung.review.ReviewActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 
 
 class HomeActivity : AppCompatActivity() {
+    companion object {
+        const val BaseUrl = "http://3.34.183.244:5000/api/"
+    }
+
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,24 +39,29 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerView() {
-        val testItems = listOf<RankingResponse.Data>(
-            RankingResponse.Data(
-                "60bc69e2af62b1dbc7d41a4e",
-                4,
-                "페리페라",
-                "올테이크팔레트",
-                "https://soptseminar5test.s3.ap-northeast-2.amazonaws.com/rank4_picture.png",
-                "이 제품은 4등 제품입니다."
-            ),
-            RankingResponse.Data(
-                "60bc6a05af62b1dbc7d41a4f",
-                5,
-                "니들리",
-                "데일리 토너 패드",
-                "https://soptseminar5test.s3.ap-northeast-2.amazonaws.com/rank5_picture.png",
-                "이 제품은 5등 제품입니다."
-            )
-        )
-        binding.recyclerviewRanking.adapter = HomeItemAdapter(testItems)
+        val retrofit: Retrofit? = Retrofit.Builder().baseUrl(BaseUrl).addConverterFactory(
+            GsonConverterFactory.create()
+        ).build()
+        val service = retrofit?.create(RankingItemService::class.java)
+        val call = service?.getRankingItem()
+        call?.enqueue(object : Callback<List<RankingResponse.Data>> {
+            override fun onResponse(
+                call: Call<List<RankingResponse.Data>>,
+                response: Response<List<RankingResponse.Data>>
+            ) {
+                Log.d("태그", response.body().toString())
+                val items = response.body()!!
+                binding.recyclerviewRanking.adapter = HomeItemAdapter(items)
+            }
+
+            override fun onFailure(call: Call<List<RankingResponse.Data>>, t: Throwable) {
+                Log.d("태그", "network failed by\n$t")
+            }
+        })
+    }
+
+    interface RankingItemService {
+        @GET("list")
+        fun getRankingItem(): Call<List<RankingResponse.Data>>
     }
 }
